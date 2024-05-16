85c131830b0690fad6f4523fe45d5cb7069f5d1c
Mohammed Sitni <149593050+msitni1337@users.noreply.github.com>
2024-05-16 23:01:47 +0000

    export with no option

--- src/built-ins/export.c
+++ src/built-ins/export.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 #include "built-ins.h"
 
-size_t get_sorted_env_count()
+size_t get_exported_env_count()
 {
     t_lstenv *env_lst;
     size_t count;
@@ -26,6 +26,36 @@ size_t get_sorted_env_count()
     return count;
 }
 
+void ft_swap(t_lstenv **p1, t_lstenv **p2)
+{
+	t_lstenv *tmp;
+
+	tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+void sort_env_list(t_lstenv **env_list)
+{
+    size_t i;
+	bool is_swapped;
+
+	is_swapped = TRUE;
+	while (is_swapped == TRUE)
+	{
+        is_swapped = FALSE;
+        i = 1;
+		while (env_list[i - 1] && env_list[i])
+		{
+			if (ft_strcmp(env_list[i - 1]->key, env_list[i]->key) < 0)
+			{
+				ft_swap(&env_list[i - 1], &env_list[i]);
+				is_swapped = TRUE;
+			}
+            i++;
+		}
+	}
+}
+
 t_lstenv**get_sorted_env()
 {
     t_lstenv *env_lst;
@@ -33,7 +63,8 @@ t_lstenv**get_sorted_env()
     size_t count;
     size_t i;
 
-    count = get_sorted_env_count();
+    count = get_exported_env_count();
+    env_lst = shell.env_list;
     res = malloc(sizeof(t_lstenv) * (count + 1));
     if (res)
     {
@@ -45,31 +76,36 @@ t_lstenv**get_sorted_env()
             i++;
         }
         res[i] = NULL;
+        sort_env_list(res);
     }
     return res;    
 }
 
 int print_exported_env(t_cmd cmd)
 {
-    char *tmp;
-    char**sorted_env;
+    t_lstenv**sorted_env;
     size_t i;
 
     sorted_env = get_sorted_env();
     i = 0;
-    while ()
+    while (sorted_env && sorted_env[i])
     {
-        if (ft_strncmp(tmp, "_=", 2))
+        if (ft_strcmp(sorted_env[i]->key, "_"))
         {
             ft_putstr_fd("declare -x ", cmd.outfile);
-            write(cmd.outfile, tmp, ft_strchr(tmp, '=') + 1 - tmp);
-            ft_putchar_fd('"', cmd.outfile);
-            ft_putstr_fd(ft_strchr(tmp, '=') + 1, cmd.outfile);
-            ft_putchar_fd('"', cmd.outfile);
+            ft_putstr_fd(sorted_env[i]->key, cmd.outfile);
+            if (sorted_env[i]->is_set)
+            {
+                ft_putstr_fd("=\"", cmd.outfile);
+                ft_putstr_fd(sorted_env[i]->value, cmd.outfile);
+                ft_putchar_fd('"', cmd.outfile);
+            }
             write(cmd.outfile, "\n", 1);
         }
         i++;
     }
+    if (sort_env_list)
+        free(sort_env_list);
     return 0;
 }
 
@@ -81,7 +117,7 @@ int ft_export(t_cmd cmd)
     }
     else
     {
-        ;
+        assert(!"NOT IMPLEMENTED");
     }
 
     return 0;
